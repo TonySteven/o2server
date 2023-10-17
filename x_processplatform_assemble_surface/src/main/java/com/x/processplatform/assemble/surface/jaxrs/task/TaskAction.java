f396469f80b7f67df5f76e495307bc13d16b0349
Steven Liu <1012987522@qq.com>
2023-10-17 14:59:58 +0800

    :art: style: 更新javaDoc.

--- x_processplatform_assemble_surface/src/main/java/com/x/processplatform/assemble/surface/jaxrs/task/TaskAction.java
+++ x_processplatform_assemble_surface/src/main/java/com/x/processplatform/assemble/surface/jaxrs/task/TaskAction.java
@@ -1,21 +1,5 @@
 package com.x.processplatform.assemble.surface.jaxrs.task;
 
-import java.util.List;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.container.AsyncResponse;
-import javax.ws.rs.container.Suspended;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.MediaType;
-
 import com.google.gson.JsonElement;
 import com.x.base.core.project.annotation.JaxrsDescribe;
 import com.x.base.core.project.annotation.JaxrsMethodDescribe;
@@ -28,6 +12,14 @@ import com.x.base.core.project.jaxrs.ResponseFactory;
 import com.x.base.core.project.jaxrs.StandardJaxrsAction;
 import com.x.base.core.project.logger.Logger;
 import com.x.base.core.project.logger.LoggerFactory;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.*;
+import javax.ws.rs.container.AsyncResponse;
+import javax.ws.rs.container.Suspended;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
 
 @Path("task")
 @JaxrsDescribe("待办操作")
@@ -316,6 +308,14 @@ public class TaskAction extends StandardJaxrsAction {
 		asyncResponse.resume(ResponseFactory.getEntityTagActionResultResponse(request, result));
 	}
 
+	/**
+	 * 保存待办意见并继续流转.返回后续人工环节处理人
+	 *
+	 * @param asyncResponse async response
+	 * @param request       request
+	 * @param id            id
+	 * @param jsonElement   json element
+	 */
 	@JaxrsMethodDescribe(value = "保存待办意见并继续流转.返回后续人工环节处理人", action = ActionProcessing.class)
 	@POST
 	@Path("{id}/processing")
